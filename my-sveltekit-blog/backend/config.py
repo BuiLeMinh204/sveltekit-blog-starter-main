31c28dfdf03c852b22859d0db349536ba4c872c6
minh <minh44659@gmail.com>
2025-06-01 16:28:35 +0700

    update

--- my-sveltekit-blog/backend/config.py
+++ my-sveltekit-blog/backend/config.py
@@ -1,10 +1,10 @@
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-MAIL_SERVER = 'smtp.mailtrap.io'
+MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
 MAIL_PORT = 587
-MAIL_USERNAME = 'your_mailtrap_username'
-MAIL_PASSWORD = 'your_mailtrap_password'
+MAIL_USERNAME = '89ceb0639fef2d'
+MAIL_PASSWORD = '53bb6ec47d3fcc'
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 MAIL_DEFAULT_SENDER = 'noreply@example.com'
