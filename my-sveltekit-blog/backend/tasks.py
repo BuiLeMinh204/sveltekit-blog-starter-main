7b24e50d293e9bcc58ddcc53b06b51215281615e
minh <minh44659@gmail.com>
2025-06-02 15:16:36 +0700

    update

--- my-sveltekit-blog/backend/tasks.py
+++ my-sveltekit-blog/backend/tasks.py
@@ -2,6 +2,12 @@ from celery import Celery
 from flask import Flask
 from flask_mail import Mail, Message
 from config import *
+import os
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
+
+celery = Celery("tasks", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 flask_app = Flask(__name__)
 flask_app.config.update(
@@ -14,7 +20,6 @@ flask_app.config.update(
 )
 
 mail = Mail(flask_app)
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 @celery.task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={'max_retries': 5})
 def send_email(self, to, subject, body):
